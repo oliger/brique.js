dc97877f8d959101c98237c908f290b21aec729d
Jimmy Oliger <hi@jimmyoliger.com>
2013-05-30 02:16:42 +0200

    Clean up Gruntfiles

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,16 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['clean', 'connect', 'watch']);
+  grunt.registerTask('default', ['connect']);
 
   grunt.initConfig({
-    clean: ['tmp'],
-
     connect: {
       server: {
-        options: { port: 8000 }
+        options: {
+          port: 8000,
+          keepalive: true
+        }
       }
-    },
-
-    watch: {
-      files: ['lib/**', 'test/**'],
-      tasks: ['clean']
     }
-
-    //transpile: {
-      //globals: {
-        //type: 'globals',
-        //imports: {
-          //'brique/dom_utils': 'window',
-          //'brique/string_utils': 'window'
-        //},
-        //files: {
-          //'tmp/brique.js': ['lib/brique.js'],
-          //'tmp/brique/string_utils.js': ['lib/brique/string_utils.js'],
-          //'tmp/brique/dom_utils.js': ['lib/brique/dom_utils.js']
-        //}
-      //}
-    //}
   });
 };
